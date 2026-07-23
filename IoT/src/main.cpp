aba13a6e4af675d7b625ec50de39d5db6ce170c2
Lachlan C <136399315+yeet230@users.noreply.github.com>
2026-07-23 12:32:14 +1000

    add code to main.cpp

--- IoT/src/main.cpp
+++ IoT/src/main.cpp
@@ -1,2 +1,15 @@
 #include <Arduino.h>
 
+void setup()
+{
+    pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+    digitalWrite(13, HIGH);
+    delay(1000);
+    digitalWrite(13, LOW);
+    delay(1000);
+    delay(100);
+}
