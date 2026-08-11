966d4e54e00c06f0b943555b6426d3640fbaa21c
Lachlan C <136399315+yeet230@users.noreply.github.com>
2026-08-11 15:24:18 +1000

    add tempreature reading functionality

--- IoT/src/main.cpp
+++ IoT/src/main.cpp
@@ -4,7 +4,28 @@ const char *mqttClient = "ESP32_Lachlan"; // EDIT THIS FIELD
 const char *mqttTopic;
 
 #include <Arduino.h>
+#include <Wire.h>
+
 #include "comms.h"
+#include "Adafruit_ADT7410.h"
+
+Adafruit_ADT7410 tempsensor = Adafruit_ADT7410();
+
+void sensor_setup()
+{
+    Serial.println("ADT7410 demo");
+
+    // Make sure the sensor is found, you can also pass in a different i2c
+    // address with tempsensor.begin(0x49) for example
+    if (!tempsensor.begin())
+    {
+        Serial.println("Couldn't find ADT7410!");
+        while (1);
+    }
+
+    // sensor takes 250 ms to get first readings
+    delay(250);
+}
 
 void performActionBasedOnPayload(String payload)
 {
@@ -24,18 +45,32 @@ void performActionBasedOnPayload(String payload)
     }
 }
 
+float tempSensorUpdate()
+{
+    // Read and print out the temperature, then convert to *F
+    float tempInC = tempsensor.readTempC();
+   
+    Serial.println(tempInC);
+
+    //delay(1000);
+    return tempInC;
+}
+
 void setup()
 {
     pinMode(LED_BUILTIN, OUTPUT);
     Serial.begin(9600);
-    
+    delay(2000);
+
     wifiSetup();
     mqttSetup();
+    sensor_setup();
 
     while (!Serial)
     {
         delay(10);
     }
+
     delay(1000);
 }
 
@@ -43,7 +78,6 @@ void loop()
 {
     // 1. Maintain connection to the broker
     mqttConnect();
-    
 
     // 2. Transmit periodic telemetry (if required by design specification)
     unsigned long now = millis();
@@ -52,18 +86,21 @@ void loop()
         lastUpdate = now;
         Serial.println("Update data now");
         // TODO: Insert customized sendDataToServer() calls here.
-        int randomNum = random(1, 10001);
-        sendDataToServer("sensorData", String(randomNum));
-    }
+        int randomNum = random(0, 100);
+        float temp = tempSensorUpdate();
 
+        String dataToUpload = "The tempreture is: " + String(temp) + "°C";
+
+        sendDataToServer("sensorData", dataToUpload);
+    }
 
     // 3. Yield execution time for PubSubClient processing
     client.loop();
     delay(100);
 }
 
-//mqttClient is the device name eg ESP32_Lachlan
-//MQTT Topics
-//EventLog/mqttClient       - For Device events eg startup, error   | Uploads
-//sensorData/mqttClient     - For Telementry Data                   | Uploads
-//devicePayload/mqttClient  - For Giving Device Data To Act On      | Recives
+// mqttClient is the device name eg ESP32_Lachlan
+// MQTT Topics
+// EventLog/mqttClient       - For Device events eg startup, error   | Uploads
+// sensorData/mqttClient     - For Telementry Data                   | Uploads
+// devicePayload/mqttClient  - For Giving Device Data To Act On      | Recives
